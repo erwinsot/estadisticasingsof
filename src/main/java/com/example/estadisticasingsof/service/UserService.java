0d1101c7eb6ef77d6328f101a1c0d82aa7f76245
Duban <esoto@unal.edu.co>
2021-06-02 23:59:57 -0500

    12 pm

--- src/main/java/com/example/estadisticasingsof/service/UserService.java
+++ src/main/java/com/example/estadisticasingsof/service/UserService.java
@@ -4,12 +4,10 @@ import com.example.estadisticasingsof.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface UserService {
     public Iterable<User> findAll();
     public Page<User> findAll(Pageable pageable);
-    public Optional<User> findById(Long id);
+    public User findById(Long id) throws Exception;
     public User save(User user);
     public  void deleteById(Long id);
     public void insert();
